03acb04ebb3687dae997346329a1ed0e5efd9dea
heynan <qajrgz10@amail.club>
2017-12-27 15:08:25 +0300

    Quiet function to convert text to uri

--- uri.c
+++ uri.c
@@ -47,6 +47,42 @@ parse_uri(const char *s, UriUriA *urip)
 	}
 }
 
+static int
+parse_uri_quiet(const char *s, UriUriA *urip)
+{
+  UriParserStateA state;
+
+  state.uri = urip;
+  uriParseUriA(&state, s);
+
+  if (state.errorCode == URI_SUCCESS) {
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
+PG_FUNCTION_INFO_V1(uri_in_quiet);
+Datum
+uri_in_quiet(PG_FUNCTION_ARGS)
+{
+  char *s = PG_GETARG_CSTRING(0);
+  uritype *vardata;
+  UriUriA uri;
+
+  int result = parse_uri_quiet(s, &uri);
+  uriFreeUriMembersA(&uri);
+
+  if (result == 0) {
+    vardata = (uritype *) cstring_to_text(s);
+    PG_RETURN_URI_P(vardata);
+  }
+  else {
+    PG_RETURN_NULL();
+  }
+}
+
 PG_FUNCTION_INFO_V1(uri_in);
 Datum
 uri_in(PG_FUNCTION_ARGS)
